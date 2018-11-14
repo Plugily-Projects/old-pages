9ee3778edb6f035246b5f8fb8e19613d4ec655d9
Plajer <worthplay.pt@onet.pl>
2018-11-14 17:36:54 +0100

    Improved cache impl

--- translate/inc/json_localization.php
+++ translate/inc/json_localization.php
@@ -1,4 +1,6 @@
 <?php
+include_once('cache.php');
+
 // the original of this code can be found on:
 // http://www.mind-it.info/2010/02/22/a-simple-approach-to-localization-in-php/
 function localize($phrase) {
@@ -7,24 +9,14 @@ function localize($phrase) {
     if (is_null($translations)) {
         $locale = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE'])[0];
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"https://plajer.xyz/localeservice/fetch.php");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "pass=localeservice&type=Pages&locale=" . $locale);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $server_output = curl_exec($ch);
+        $server_output = getCachedContent('https://plajer.xyz/localeservice/fetch.php', 'pass=localeservice&type=Pages&locale=' . $locale,
+            'locale_' . $locale);
 
         //use default locale if not found
-        if($server_output == "" || isset($_COOKIE["preferred_default_locale"])) {
-            $locale = "en-GB";
-            curl_setopt($ch, CURLOPT_URL,"https://plajer.xyz/localeservice/fetch.php");
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, "pass=localeservice&type=Pages&locale=" . $locale);
-
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $server_output = curl_exec($ch);
+        if($server_output == '' || isset($_COOKIE['preferred_default_locale'])) {
+            $locale = 'en-GB';
+            $server_output = getCachedContent('https://plajer.xyz/localeservice/fetch.php', 'pass=localeservice&type=Pages&locale=' . $locale,
+                'locale_' . $locale);
         }
 
         /* Load the language file as a JSON object
--- translate/inc/poeditor_reader.php
+++ translate/inc/poeditor_reader.php
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+include_once('cache.php');
 
 function readLanguages($projectId) {
     static $cache = NULL;
@@ -25,14 +26,8 @@ function readLanguages($projectId) {
         }
         $apiKey = file_get_contents(__DIR__ . '/POEditorAPIKey.txt');
 
-        $curl = curl_init('https://api.poeditor.com/v2/languages/list');
-        curl_setopt($curl, CURLOPT_POST, true);
-
-        curl_setopt($curl, CURLOPT_POSTFIELDS, 'api_token=' . $apiKey . '&id=' . $projectId);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
-        curl_close($curl);
-
+        $response = getCachedContent('https://api.poeditor.com/v2/languages/list', 'api_token=' . $apiKey . '&id=' . $projectId,
+            'poeditor_proj' . $projectId);
         $cache[$projectId] = $response;
     }
     return json_decode($cache[$projectId]);
