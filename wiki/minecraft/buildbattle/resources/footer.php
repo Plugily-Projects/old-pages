f8bbbe61ead54fc04172c3b76dad6788f281691e
Plajer <plajer.xyz@gmail.com>
2019-05-02 12:17:06 +0200

    Include footer resource

---
+++ wiki/minecraft/buildbattle/resources/footer.php
@@ -0,0 +1,5 @@
+<footer class="col-12 page-footer font-small elegant-color-dark p-0">
+  <div class="footer-copyright text-center py-3 h-100">© <?php echo date("Y"); ?> <a target="_blank" href="https://www.spigotmc.org/resources/44703/">Build Battle</a> |
+    Created by <a target="_blank" href="https://github.com/Plajer-Lair">Plajer's Lair</a> and maintained by <a target="_blank" href="https://www.spigotmc.org/members/423193/">Plajer</a>
+  </div>
+</footer>
